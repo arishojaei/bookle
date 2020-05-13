df289b69e742ceca91e5ea3d2ec85dd5f50321fd
Arman Shojaei <amnshojaei@gmail.com>
2020-05-13 13:48:31 +0430

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div id="app" class="container">
+
+    <!-- Navbar -->
+    <nav class="d-flex justify-content-between align-items-center py-4">
+      <router-link to="/">
+        <img src="./assets/img/logo.png" alt="brand_logo">
+      </router-link>
+      <div class="d-flex align-items-center">
+        <img height="20" src="./assets/img/user_icon.svg" alt="user_icon">
+        <span class="pr-2">سارا</span>
+      </div>
+    </nav>
+
+    <!-- Content -->
+    <router-view />
+
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="home">
+  <div class="my-5">
+
   </div>
 </template>
 
