df2c842bd1ae396345d7f8542382a2e42466ba09
Arman Shojaei <amnshojaei@gmail.com>
2020-05-13 14:19:28 +0430

    add banner

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="my-5">
+  <div class="my-4">
+
+    <div class="banner">
+      <img src="@/assets/img/banner.png" alt="">
+    </div>
 
   </div>
 </template>
-
-<script>
-export default {
-  name: 'Home',
-}
-</script>
